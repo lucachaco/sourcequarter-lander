b2d9b687ec039138d26fdf119f9ec91ba18c4d88
lucachaco <luiscarlosch@gmail.com>
2014-12-29 18:43:24 -0600

    test

--- conf/bootstrap.sh
+++ conf/bootstrap.sh
@@ -10,6 +10,19 @@ dpkg-reconfigure locales
 
 apt-get install -y git
 
-apt-get install -y ruby ruby-dev make gcc nodejs
+apt-get install -y curl
 
-gem install jekyll --no-rdoc --no-ri
+#http://sharadchhetri.com/2014/06/30/install-jekyll-on-ubuntu-14-04-lts/
+#install rvm
+curl -#LO https://rvm.io/mpapis.asc
+gpg --import mpapis.asc
+curl -sSL https://get.rvm.io | bash -s stable
+source /home/vagrant/.rvm/scripts/rvm
+rvm requirements
+#install ruby
+rvm install 2.1.2
+rvm use 2.1.2 --default
+
+apt-get install -y nodejs
+gem install jekyll
+jekyll -v
